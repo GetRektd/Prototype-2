9814ced83f8cb584b1cf5759d5f3384cc40b8340
GetRektd <lgibb24@aoiths.org>
2022-10-13 12:59:17 -0400

    2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,34 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float xRange = 10;
+    public float speed = 10.0f;
 
 
-    
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10;
+    
 
     // Update is called once per frame
     void Update()
     {
 
-        if(transform.position.x < -xRange) {
-           transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
-        if (transform.position.x > xRange)
+        //keep the player in bounds
+        if (transform.position.x < -xRange)
+        { 
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
 
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
